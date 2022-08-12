d2595065648bddf9ed855de9555f0b553ea1b577
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-08-12 14:30:24 +0200

    Add ShrinkingColoredBox

---
+++ src/main/java/widgets/ShrinkingColoredBox.java
@@ -0,0 +1,27 @@
+package widgets;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.BasicTextImage;
+import com.googlecode.lanterna.screen.Screen;
+
+public class ShrinkingColoredBox extends OneChildWidget {
+    final TextColor color;
+
+    public ShrinkingColoredBox(TextColor color, Widget widget) {
+        super(widget);
+        this.color = color;
+    }
+
+    @Override
+    public void rawRender(int x, int y, int width, int height, Screen screen) {
+        screen.newTextGraphics().drawImage(new TerminalPosition(x, y),
+                                           new BasicTextImage(new TerminalSize(width, height),
+                                                              new TextCharacter(' ', color, color)
+                                           )
+        );
+        super.rawRender(x, y, width, height, screen);
+    }
+}
